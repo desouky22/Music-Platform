971dc059b787db12f72fa21c19e15b2f6b77c92c
Omar Desouky <omaressamdesouky2274@gmail.com>
2022-11-04 22:27:01 +0200

    Finishing Task5

--- artists/serializers.py
+++ artists/serializers.py
@@ -4,15 +4,15 @@ from albums.serializers import AlbumSerializer
 
 
 class ArtistSerializer(serializers.ModelSerializer):
-    approved_albums = serializers.SerializerMethodField(
-        method_name="calculate_approved_albums"
-    )
+    # approved_albums = serializers.SerializerMethodField(
+    #     method_name="calculate_approved_albums"
+    # )
 
-    albums = AlbumSerializer(many=True)
+    # albums = AlbumSerializer(many=True)
 
-    def calculate_approved_albums(self, obj):
-        return obj.albums.filter(is_approved=True).count()
+    # def calculate_approved_albums(self, obj):
+    #     return obj.albums.filter(is_approved=True).count()
 
     class Meta:
         model = Artist
-        fields = ["stage_name", "social_link", "approved_albums", "albums"]
+        fields = ["id", "stage_name", "social_link"]
